f4930a24ec20165b1619dbde95884765ca642b47
Harshitha SV <solaiharshitha0@gmail.com>
2023-09-18 22:34:03 +0200

    some changes

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -8,7 +8,7 @@
     />
     <SearchComponent @search="handleSearch" :loading="loading" />
   </div>
-  <div v-if="IsInitialState">
+  <div v-if="isInitialState">
     <StatusMessage type="initial-state" />
   </div>
   <div v-else-if="shouldShowNoDataError">
@@ -26,66 +26,78 @@
 </template>
 
 <script>
-import CardsList from '../components/CardsList.vue'
-import SearchComponent from '../components/SearchComponent.vue'
-import FilterComponent from '../components/FilterComponent.vue'
-import { fetchData, fetchTopics } from '../api/services/mockDataService'
-import StatusMessage from '../components/StatusMessage.vue'
+import { ref, onMounted, computed } from 'vue';
+import CardsList from '../components/CardsList.vue';
+import SearchComponent from '../components/SearchComponent.vue';
+import FilterComponent from '../components/FilterComponent.vue';
+import { fetchData, fetchTopics } from '../api/services/mockDataService';
+import StatusMessage from '../components/StatusMessage.vue';
 
 export default {
   name: 'HomePage',
-  data() {
-    return {
-      loading: false,
-      error: null,
-      apiError: null,
-      searchResults: [],
-      selectedTopic: 'all',
-      topicOptions: []
-    }
-  },
-  created() {
-    this.fetchTopicOptions()
-  },
-  computed: {
-    IsInitialState() {
-      return !this.loading && !this.apiError && !this.error && this.searchResults.length === 0
-    },
-    shouldShowNoDataError() {
-      return this.searchResults.length === 0 && this.error
-    }
-  },
-  methods: {
-    async fetchTopicOptions() {
+  setup() {
+    const loading = ref(false);
+    const error = ref(null);
+    const apiError = ref(null);
+    const searchResults = ref([]);
+    const selectedTopic = ref('all');
+    const topicOptions = ref([]);
+
+    const isInitialState = computed(() => !loading.value && !apiError.value && !error.value && searchResults.value.length === 0);
+    const shouldShowNoDataError = computed(() => searchResults.value.length === 0 && error.value);
+    
+    const fetchTopicOptions = async () => {
       try {
-        const responseData = await fetchTopics()
-        this.topicOptions = ['all', ...responseData]
-      } catch (error) {
-        this.apiError = error
+        const responseData = await fetchTopics();
+        console.log(responseData);
+        topicOptions.value = ['all', ...responseData];
+      } catch (err) {
+        apiError.value = err;
       }
-    },
-    handleTopicSelected(topic) {
-      this.selectedTopic = topic;
-      this.searchResults = [];
-      this.loading = false;
-      this.error = null;
-    },
-    handleSearch(query) {
-      this.error = null
-      this.loading = true
-      fetchData(query, this.selectedTopic)
+    };
+
+    const handleTopicSelected = (topic) => {
+      selectedTopic.value = topic;
+      searchResults.value = [];
+      loading.value = false;
+      error.value = null;
+    };
+
+    const handleSearch = (query) => {
+      error.value = null;
+      loading.value = true;
+
+      fetchData(query, selectedTopic.value)
         .then((results) => {
-          this.searchResults = results;
-          this.error = results.length === 0 ? new Error('No matching data found') : null;
+          searchResults.value = results;
+          error.value = results.length === 0 ? new Error('No matching data found') : null;
         })
-        .catch((error) => {
-          this.apiError = error;
+        .catch((err) => {
+          apiError.value = err;
         })
         .finally(() => {
-          this.loading = false;
+          loading.value = false;
         });
-    }
+    };
+
+    onMounted(() => {
+      fetchTopicOptions();
+    });
+
+    return {
+      loading,
+      error,
+      apiError,
+      searchResults,
+      selectedTopic,
+      topicOptions,
+      isInitialState,
+      shouldShowNoDataError,
+      handleTopicSelected,
+      handleSearch,
+    };
   },
+
   components: {
     SearchComponent,
     CardsList,
